891e6b7a0a0a5deb4e8e6f69eaa247a916bf8ff6
Mobilnik <iaulitin@yandex.ru>
2021-06-19 22:21:55 +0300

    Code cleanup 2

---
+++ api-goodbye-service/src/main/java/ru/milandr/apiservice2/GoodbyeController.java
@@ -0,0 +1,16 @@
+package ru.milandr.apiservice2;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/goodbye")
+public class GoodbyeController {
+
+    @GetMapping
+    public ResponseEntity<String> request1() {
+        return ResponseEntity.ok("Goodbye from service 2");
+    }
+}
---
+++ api-hello-service/src/main/java/ru/milandr/apiservice1/ApiService1Application.java
@@ -0,0 +1,15 @@
+package ru.milandr.apiservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiService1Application.class, args);
+    }
+
+}
